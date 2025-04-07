7595e349a82656df451a2784081eab596e94b03d
ItzdaveArch190 <tlga.davebesorio@gmail.com>
2025-04-07 18:44:25 +0800

    Student.java

--- ACTIVITIES/Student.java
+++ ACTIVITIES/Student.java
@@ -1,1 +1,14 @@
+public class Students extends Person {
 
+    String GradeLevel;
+
+    public Students(String name, int id, String GradeLevel) {
+        super(name, id);
+        this.GradeLevel = GradeLevel;
+    }
+    @Override
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Grade Level: " + GradeLevel);
+    }
+}
