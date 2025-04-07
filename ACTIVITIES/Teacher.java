ce68fb94950d4d89384a8971e59161d73e8ce4f3
ItzdaveArch190 <tlga.davebesorio@gmail.com>
2025-04-07 18:44:55 +0800

    Teacher.java

--- ACTIVITIES/Teacher.java
+++ ACTIVITIES/Teacher.java
@@ -1,1 +1,13 @@
+public class Teacher extends Person{
 
+    String subject;
+    Teacher(String name, int id, String subject) {
+        super(name, id);
+        this.subject = subject;
+    }
+    @Override
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Subject: " + subject); 
+   }
+}
